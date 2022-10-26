25547319dd4510f87be7ed8c0fc94139e7efa9c3
micha003 <michael.vorogushyn@wbgym.de>
2022-10-26 23:14:13 +0200

    edit input_kk and createKKs.js

---
+++ src/js/createKKs.js
@@ -0,0 +1,11 @@
+function createKK() {
+    var kk_v = String(window.document.getElementById("kk_vorderseite").value);
+    var kk_r = String(window.document.getElementById("kk_rückseite").value);
+
+    //test
+    alert(kk_v + " " + kk_r);
+    return kk_v, kk_r;
+}
+
+// test
+alert("TEST");
