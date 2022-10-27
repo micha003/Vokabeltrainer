2975b0fcb16c0b5ae6dbc25a806c89bb06edcecb
micha003 <michael.vorogushyn@wbgym.de>
2022-10-27 19:17:44 +0200

    developing function to add KK's to a temp array
    Then after the adding to temp and "submitting" them, the .txt-file will be created (hopefully)

--- src/js/createKKs.js
+++ src/js/createKKs.js
@@ -1,20 +1,31 @@
 function getInput_kk() {
   var kk_v = document.input_kk.input_kk_v.value;
-  var kk_r = document.input_kk.input_kk_r.value; 
+  var kk_r = document.input_kk.input_kk_r.value;
 
-  alert(kk_v + " "+ kk_r);
-  return kk_v, kk_r;
+  // still_input is necessary for the while-loop
+  still_input = true;
+
+  while(still_input) {
+    var userinput = String(kk_v + ": " + kk_r);
+
+    // Append the userinput to a temp. storage (Array)
+    temp = []
+    temp.push(userinput);
+
+    if (document.input_kk.finish_c_kk.onclick) {
+      still_input = false;
+    } else {}
+  }
+
+  //test
+  alert(temp);
 }
 
+/*
 function WriteIntxt(kk_v, kk_r) {
-  var userinput = String(kk_v + ": " + kk_r);
-  var blob = new Blob([userinput], { type: "text/plain;charset=utf-8"});
+  
+  var blob = new Blob([temp], { type: "text/plain;charset=utf-8"});
 
-  saveAs(blob, "files/vocab.txt");
+  saveAs(blob, "vocab.txt");
 }
-
-function main() {
-  getInput_kk();
-
-  WriteIntxt(getInput_kk.kk_v, getInput_kk.kk_r);
-}
+*/
