2196d302b4a9d88d738b10ae2eb92fadd3398d55
micha003 <michael.vorogushyn@wbgym.de>
2022-10-27 18:32:48 +0200

    started developing of function to create .txt

--- src/js/createKKs.js
+++ src/js/createKKs.js
@@ -1,7 +1,20 @@
 function getInput_kk() {
-    var kk_v = document.input_kk.input_kk_v.value;
-    var kk_r = document.input_kk.input_kk_r.value; 
+  var kk_v = document.input_kk.input_kk_v.value;
+  var kk_r = document.input_kk.input_kk_r.value; 
 
-    alert(kk_v + " "+ kk_r);
-    return kk_v, kk_r;
-  }
+  alert(kk_v + " "+ kk_r);
+  return kk_v, kk_r;
+}
+
+function WriteIntxt(kk_v, kk_r) {
+  var userinput = String(kk_v + ": " + kk_r);
+  var blob = new Blob([userinput], { type: "text/plain;charset=utf-8"});
+
+  saveAs(blob, "files/vocab.txt");
+}
+
+function main() {
+  getInput_kk();
+
+  WriteIntxt(getInput_kk.kk_v, getInput_kk.kk_r);
+}
