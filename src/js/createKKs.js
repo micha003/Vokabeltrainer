8685ad94806a1e422aec01f9e05dda820eda270c
micha003 <michael.vorogushyn@wbgym.de>
2022-10-28 12:38:44 +0200

    developing createKKs.js
    it's just a milestone to jump back, if it's completely going wrong

--- src/js/createKKs.js
+++ src/js/createKKs.js
@@ -1,31 +1,32 @@
+temp = new Array();
+create_txt = false
+
+function get_status_ckk(variable) {
+  variable = true;
+  return variable;
+}
+
 function getInput_kk() {
   var kk_v = document.input_kk.input_kk_v.value;
   var kk_r = document.input_kk.input_kk_r.value;
 
-  // still_input is necessary for the while-loop
-  still_input = true;
+  var userinput = String(kk_v + ": " + kk_r + "\n");
 
-  while(still_input) {
-    var userinput = String(kk_v + ": " + kk_r);
+  // test
+  alert(userinput);
 
-    // Append the userinput to a temp. storage (Array)
-    temp = []
-    temp.push(userinput);
-
-    if (document.input_kk.finish_c_kk.onclick) {
-      still_input = false;
-    } else {}
-  }
-
-  //test
+  // Append the userinput to a temp. storage (Array)
+  temp.push(userinput);
+  // test
   alert(temp);
-}
-
-/*
-function WriteIntxt(kk_v, kk_r) {
   
-  var blob = new Blob([temp], { type: "text/plain;charset=utf-8"});
+  if (create_txt) {
+    var blob = new Blob([temp_array], { type: "text/plain;charset=utf-8"});
 
-  saveAs(blob, "vocab.txt");
+     saveAs(blob, "lernset.txt");
+    temp = [];
+    return temp;
+  }
 }
-*/
+
+
