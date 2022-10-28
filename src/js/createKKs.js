3eed0d6a698175ba1ce8d2b812cf14eb5cc4bedf
micha003 <michael.vorogushyn@wbgym.de>
2022-10-28 16:54:35 +0200

    defined new main function
     to put single funs together

--- src/js/createKKs.js
+++ src/js/createKKs.js
@@ -1,6 +1,3 @@
-// recursive function
-temp = [];
-
 function getInput_kk() {
   var kk_v = document.input_kk.input_kk_v.value;
   var kk_r = document.input_kk.input_kk_r.value;
@@ -12,14 +9,27 @@ function getInput_kk() {
   return userinput;
 }
 
-function push_to_temp(userinput) {
+function push_to_temp(userinput, temp) {
   temp.push(userinput);
+}
+
+function create_array(temp) {
+  // create var for return value of first function
+  var userinput_temp = getInput_kk();
+  console.log(userinput_temp); //test
+  // pushes the userinput to the array
+  var temp = push_to_temp(userinput_temp);
   return temp;
 }
 
 function create_txt(temp) {
   var blob = new Blob([temp], { type: "text/plain;charset=utf-8" });
   saveAs(blob, "set.txt");
+
+  temp = [];
 }
 
-
+function main(temp) {
+  var final_temp = create_array(temp);
+  create_txt(final_temp);
+}
