301037849aacf4b404bc1bc4714cae0c4b173042
micha003 <michael.vorogushyn@wbgym.de>
2022-10-31 08:34:46 +0100

    implemented choose the name of the file

--- src/js/createKKs.js
+++ src/js/createKKs.js
@@ -1,9 +1,9 @@
 var temp = new Array();
 var str_temp = new String();
 
-function getInput_kk(array = Array()) { // the only function which is working right now :(
-  var kk_v = document.input_kk.input_kk_v.value;
-  var kk_r = document.input_kk.input_kk_r.value;
+function getInput_kk() { // the only function which is working right now :(
+  var kk_v = document.getElementById("kk_v").value;
+  var kk_r = document.getElementById("kk_r").value;
 
   var userinput = String("\n" + kk_v + ": " + kk_r);
 
@@ -21,6 +21,10 @@ function create_and_save() {
   // test
   console.log(temp);
 
-  var blob = new Blob([String(temp).split(",")], { type: "text/plain;charset=utf-8" });
-  saveAs(blob, "set.txt");
+  var chosen_set = document.getElementById("choose_set").value;
+
+  var blob = new Blob([temp], { type: "text/plain;charset=utf-8" });
+  saveAs(blob, chosen_set + ".txt");
+
+  temp = [];
 }
