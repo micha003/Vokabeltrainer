42aea6d95d5583fd76fdae02a450622362c5a3ce
micha003 <michael.vorogushyn@wbgym.de>
2022-11-02 09:49:51 +0100

    started fixing #15

--- src/js/createKKs.js
+++ src/js/createKKs.js
@@ -2,12 +2,30 @@
 var temp = new Array();
 
 function getInput_kk() {
+  // saves the value for the set name
+  var chosen_set = document.getElementById("choose_set");
+
+  if (chosen_set !== undefined && chosen_set !== null && chosen_set !== "") {
+
+  } else {
+    var none_inp = true;
+
+    do {
+      chosen_set = prompt("Bitte geben Sie einen Namen für das Lernset ein:");
+      if (chosen_set !== undefined && chosen_set !== null && chosen_set !== "") {
+        none_inp = false;
+      } else {
+        continue;
+      }
+    } while (none_inp);
+  }
+
   // saves the values of the entry fields for the KK'S
   var kk_v = document.getElementById("kk_v").value;
   var kk_r = document.getElementById("kk_r").value;
 
   //produce a String to put the single var together
-  var userinput = String("\n" + kk_v + ": " + kk_r);
+  var userinput = String(kk_v + ": " + kk_r + "\n");
 
   //saves the KK to the localstorage
   save_to_webstorage(userinput);
@@ -16,9 +34,6 @@ function getInput_kk() {
 }
 
 function create_and_save() {
-  // executes the function above
-  getInput_kk(temp);
-
   // takes the value of an input field and set it as the file name
   var chosen_set = document.getElementById("choose_set").value;
 
