7fa46ebb90322eae09863151a142e67a802c6d5d
micha003 <michael.vorogushyn@wbgym.de>
2022-11-30 11:57:27 +0100

    fixed #22

--- src/js/import_txt.js
+++ src/js/import_txt.js
@@ -4,9 +4,15 @@ file.addEventListener('change', function () {
   const reader = new FileReader();
   reader.onload = function (progressEvent) {
     console.log(this.result);
+
+    let input = this.result;
+    // check the type of the result
+    let checkType = typeof this.result;
+    console.log(checkType);
+    // try to split it up, what worked :)
+    let splitedup = input.split('\n');
+    console.log(splitedup);
   };
-  const full_file = reader.readAsText(this.files[0]);
+  let full_file = reader.readAsText(this.files[0]);
   console.log(full_file);
-  let splitup = full_file.split("\n");
-  console.log(splitup);
 });
