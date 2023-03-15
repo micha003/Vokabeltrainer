07c7ece0e4dfa998e694f58b9aa7c720be5ee467
micha003 <michael.vorogushyn@wbgym.de>
2023-03-15 07:41:35 +0100

    stats is working!

--- src/python/practice.py
+++ src/python/practice.py
@@ -5,70 +5,45 @@ import random as r
 import time as t
 
 
-def getQueriedSet(allSets):
+def getQueriedSet(allSets: dict) -> tuple:
     chosenSetName = input("Bitte wählen Sie ein importiertes Set aus: ")
 
-    allKeys = []
-    for key in allSets:
-        allKeys.append(key)
-
-    if chosenSetName not in allKeys:
-        falseSet = True
-        while falseSet:
-            print("Dieses Set wurde nicht importiert. Wenn Sie dies nachträglich tun wollen, dann geben Sie 'import()' ein.")
+    if chosenSetName in allSets.keys():
+        chosenSet = allSets[chosenSetName]
+        return chosenSet, chosenSetName
+    else:
+        while True:
             chosenSetName = input(
                 "Bitte wählen Sie ein importiertes Set aus: ")
-
-            if chosenSetName in allKeys:
-                falseSet = False
-            elif chosenSetName == "import()":
-                return
+            if chosenSetName in allSets.keys():
+                chosenSet = allSets[chosenSetName]
+                return chosenSet, chosenSetName
             else:
-                continue
-    elif chosenSetName == "import()":
-        return
-    else:
-        pass
+                print("Das Set existiert nicht!")
 
-    chosenSet = allSets[chosenSetName]
-    # simple tests
-    print(chosenSet)
-    print(chosenSetName)
-    return chosenSet, chosenSetName
 
-def getQuerriedKK(Set: list, currentIndex: int):
+def getQuerriedKK(Set: list, currentIndex: int) -> tuple:
     kkV = Set[currentIndex].split(":")[0]
     kkR = Set[currentIndex].split(":")[1]
-
-    print(Set)
-    print(currentIndex)
-    print(kkV)
-    print(kkR)
     return kkV, kkR
 
 
-def Querry(allSets):
-    value = getQueriedSet(allSets)[0]
+def Querry(allSets: dict):
+    value = getQueriedSet(allSets)
     chosenSet = value[0]
     chosenSetName = value[1]
 
     indizes = [i for i in range(len(chosenSet))]
-    # Test
-    print(indizes)
     r.shuffle(indizes)
-    print(indizes)
-
 
     rightAnswers = 0
     dkAnswers = 0  # Der abgefragte wusste die Antwort gar nicht
     totalAns = len(chosenSet)
     falseAnswers = 0
 
-
     for a in indizes:
-        fullKK = getQuerriedKK(chosenSet, a)
-        kkV = fullKK[0]
-        kkR = fullKK[1]
+        kkV = getQuerriedKK(chosenSet, a)[0]
+        kkR = getQuerriedKK(chosenSet, a)[1]
 
         givenAnswer = input(f"{kkV}: ").strip()
 
@@ -90,15 +65,15 @@ def Querry(allSets):
                 - {falseAnswers} falsch
             """)
 
-    statsFile = open(f"{chosenSetName}_stats.csv", "a")
-    statsFile.write(
-        f"{str(t.strftime('%d.%m.%Y %H:%M'))}, {totalAns}, {rightAnswers}, {dkAnswers}, {falseAnswers}\n")
-    statsFile.close()
-    c.horizontalLine()
+    with open(f"{chosenSetName}_stats.csv", "a") as statsFile:
+        statsFile.write(
+            f"{str(t.strftime('%d.%m.%Y %H:%M'))}, {totalAns}, {rightAnswers}, {dkAnswers}, {falseAnswers}\n")
+        c.horizontalLine()
 
 
 if __name__ == "__main__":
-    AIS = {"Test": ["Hallo:Hello", "Major:Haupt", "Remis:Unentschieden"]}
+    AIS = {"Test": ["1:2", "2:1", "3:4", "4:3",
+                    "5:6", "6:5", "7:8", "8:7", "9:10", "10:9"]}
     Querry(AIS)
-    #getQueriedSet(AIS)
-    getQuerriedKK(AIS["Test"], 1)
+    # getQueriedSet(AIS)
+    # getQuerriedKK(AIS["Test"], 1)
